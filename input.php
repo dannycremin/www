f480f7d8780f8687acb1cf66cdbe2dd9c1e08117
dannycremin <dannycreminradio@gmail.com>
2018-02-28 23:48:50 +0000

    Added variable input for .bit + input.php script

---
+++ input.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Blockchain / PowerDNS Query Engine</h2>
+
+
+  <form action="reply.php" method="post">
+    <label for="fname">Enter a .bit address to resolve</label><br>
+    <input type="text" id="dotbit" name="dotbit" placeholder="Example: dotbitwhois">
+	<br>
+
+<label for="lname">Enter a top level domain address to resolve</label><br>
+    <input type="text" id="icann" name="icann" placeholder="Example: google.com">
+	<br>
+    <input type="submit" value="Submit">
+
+  </form>
+
+</body>
+</html>
--- reply.php
+++ reply.php
@@ -1,9 +1,10 @@
 <?php
-// $dotbit= $_POST["dotbit"];
-$output = shell_exec("sudo /usr/bin/namecoind name_show d/dannycremin 2>&1");
+$dotbit= $_POST["dotbit"];
+$output = shell_exec("sudo /usr/bin/namecoind name_show d/$dotbit 2>&1");
 echo "The IP to name mapping for <b>$dotbit.bit</b>";
 $json=$output;
 $djson= json_decode($json);
 $test= str_replace("\"", "", $djson->value);
+echo "<br><br>";
 echo $test;
 ?>
