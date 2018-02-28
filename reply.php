e93517dc64664c104285121719791d299ccfd5ee
dannycremin <dannycreminradio@gmail.com>
2018-02-28 23:57:43 +0000

    Added PDNS query backend

--- reply.php
+++ reply.php
@@ -8,3 +8,15 @@ $test= str_replace("\"", "", $djson->value);
 echo "<br><br>";
 echo $test;
 ?>
+
+<?php
+$icann=$_POST["icann"];
+$output = shell_exec("dig   $icann  2>&1");
+echo "The IP to name mapping for  <b>$icann</b> ";
+echo  "<br><br>";
+echo $output;
+echo"<br><br>";
+$linkaddress = "https:\/\/".$icann;
+echo "<a href='$linkaddress'>Click here to go to $icann</a>";
+echo "<br><br>";
+?>
