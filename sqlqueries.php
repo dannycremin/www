3d6731f5a2fb64cf42bf5cd8555c6ab8e176e7ae
dannycremin <dannycreminradio@gmail.com>
2018-03-01 12:15:23 +0000

    Added userinput variables into SQL queries/inserts

--- sqlqueries.php
+++ sqlqueries.php
@@ -14,4 +14,22 @@ AS tmp WHERE NOT EXISTS (SELECT * FROM records WHERE name="dannycremin.bit" AND 
 INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', 'dannycremin.bit', '50.40.20.30','A',86400,NULL)
 AS tmp WHERE NOT EXISTS (SELECT * FROM records WHERE name="dannycremin.bit" AND type="A");
 
+
+// Added the ability to take user input for SOA record and name.
+
+INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', '$dotbiweboutput', 'localhost localhost 1','SOA',86400,NULL)
+AS tmp WHERE NOT EXISTS (SELECT * FROM records WHERE name="$dotbitweboutput" AND type="SOA");
+
+// Added ability to take user input for A record, name and IP.
+
+INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', '$dotbitweboutput', '$ipoutput','A',86400,NULL)
+AS tmp WHERE NOT EXISTS (SELECT * FROM records WHERE name="$dotbitweboutput" AND type="A");
+
+
+
+
+$sql = "INSERT INTO records (domain_id, name, content, type, ttl, prio)
+VALUES (2,'$dotbitweboutput','localhost localhost 1','SOA',86400,NULL),
+	   (2,'$dotbitweboutput','$ipoutput','A',120,NULL)";
+
 ?>
