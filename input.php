a73b6e5da3eb6bbc3ef3cc530ba856fe847bef9e
dannycremin <dannycreminradio@gmail.com>
2018-03-01 20:08:51 +0000

    Fixed variable name userinput > queryinput

--- input.php
+++ input.php
@@ -17,7 +17,7 @@
     <input type="text" id="wildcardtextbox" name="wildcardtextbox" placeholder="Example: google.com/dannycremin.bit">
 	<br>
 	<label for="wildcardbox">Enter a top level domain address to resolve</label><br>
-    <input type="text" id="userinput" name="userinput" placeholder="Example: google.com/dannycremin.bit">
+    <input type="text" id="queryinput" name="queryinput" placeholder="Example: google.com/dannycremin.bit">
 	<br>
     <input type="submit" value="Submit">
 
