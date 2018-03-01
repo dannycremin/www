684159b05401db58b51a751bbb4b9ab9137f1799
dannycremin <dannycreminradio@gmail.com>
2018-03-01 00:19:11 +0000

    Updated Namecoin request variables
    New name $ipoutput and requested back the $dotbit name with .bit appended.

--- reply.php
+++ reply.php
@@ -4,9 +4,11 @@ $output = shell_exec("sudo /usr/bin/namecoind name_show d/$dotbit 2>&1");
 echo "The IP to name mapping for <b>$dotbit.bit</b>";
 $json=$output;
 $djson= json_decode($json);
-$test= str_replace("\"", "", $djson->value);
+$ipoutput= str_replace("\"", "", $djson->value);
 echo "<br><br>";
-echo $test;
+echo $ipoutput;
+echo "<br><br>";
+echo "$dotbit . bit";
 ?>
 
 <?php
@@ -34,3 +36,4 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?>
+
