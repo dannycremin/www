9cd654c5bc3d4814e87407634679c77f72240d2f
dannycremin <dannycreminradio@gmail.com>
2018-03-01 12:32:35 +0000

    Typo in the $dotbitweboutput variable

--- reply.php
+++ reply.php
@@ -41,7 +41,7 @@ if (!$conn) {
 // VALUES (2,'$dotbitweboutput','localhost localhost 1','SOA',86400,NULL),
 //   (2,'$dotbitweboutput','$ipoutput','A',120,NULL)";
 	   
-$sql = "INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', '$dotbiweboutput', 'localhost localhost 1','SOA',86400,NULL)
+$sql = "INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', '$dotbitweboutput', 'localhost localhost 1','SOA',86400,NULL)
 AS tmp WHERE NOT EXISTS (SELECT * FROM records WHERE name='$dotbitweboutput' AND type="SOA")";	   
 
 // $sql2 = "INSERT INTO records (domain_id, name, content, type, ttl, prio) SELECT * FROM (SELECT '2', '$dotbitweboutput', '$ipoutput','A',86400,NULL)
