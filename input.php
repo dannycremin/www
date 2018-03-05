437e997cf384d796a679a7e165a413c03552e325
dannycremin <dannycreminradio@gmail.com>
2018-03-05 11:17:51 +0000

    Removed additional input boxes

--- input.php
+++ input.php
@@ -6,7 +6,8 @@
 
 
   <form action="reply.php" method="post">
-    <label for="fname">Enter a .bit address to resolve</label><br>
+    <!--
+	<label for="fname">Enter a .bit address to resolve</label><br>
     <input type="text" id="dotbit" name="dotbit" placeholder="Example: dotbitwhois">
 	<br>
 
@@ -16,8 +17,12 @@
 	<label for="wildcardbox">Enter a top level domain address to resolve</label><br>
     <input type="text" id="wildcardtextbox" name="wildcardtextbox" placeholder="Example: google.com/dannycremin.bit">
 	<br>
-	<label for="queryinput">Enter a top level domain address to resolve</label><br>
+	-->
+	<br>
+	<label for="queryinput">Enter an address to be resolved</label><br>
+	<br>
     <input type="text" id="queryinput" name="queryinput" placeholder="Example: google.com/dannycremin.bit">
+	<br>
 	<br>
     <input type="submit" value="Submit">
 
