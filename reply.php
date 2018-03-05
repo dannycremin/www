f5e631e808f3b77dc3b41cc6ebe1600ecf637a6a
dannycremin <dannycreminradio@gmail.com>
2018-03-05 17:01:39 +0000

    Added useful heading for TLD request reply

--- reply.php
+++ reply.php
@@ -112,7 +112,7 @@ if (strpos($queryentry, '.bit') !== false) {
 } else {
 	
 	$tldqueryresult = shell_exec("dig   $queryentry  2>&1");
-	echo "<h3>.bit TLD statement result</h3>";
+	echo "<h3>DNS information for $queryentry</h3>";
 	echo  "<br><br>";
 	echo "<pre>$tldqueryresult</pre>";	
 }
