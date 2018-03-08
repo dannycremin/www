854f1159796c33007e3e24752bc733dcae9afadc
dannycremin <dannycreminradio@gmail.com>
2018-03-08 22:14:47 +0000

    Added testing echo's for .bit dig

--- reply.php
+++ reply.php
@@ -7,9 +7,11 @@
 $queryentry= $_POST["queryinput"];
 
 if (strpos($queryentry, '.bit') !== false) {
+	
 	// If user enters www. on record strip it and pass to $querystripwww variable
 	
 	// TESTING CODE BELOW
+	
 	echo "<b>Output before www. removed</b>";
 	echo "<br><br>";
 	echo $queryentry;
@@ -20,10 +22,12 @@ if (strpos($queryentry, '.bit') !== false) {
 	$querystripwww= str_replace("www.", "", $queryentry);
 	
 	// TESTING CODE BELOW
+	
 	echo "<b>Output after www. removed</b>";
 	echo "<br><br>";
 	echo $querystripwww;
 	echo "<br><br>";
+	
 	// TESTING CODE ENDS
 	
 	
@@ -35,19 +39,29 @@ if (strpos($queryentry, '.bit') !== false) {
 	echo "<br><br>";
 	echo $querystripwww;
 	echo "<br><br>";
+	
 	// TESTING CODE ENDS
 	
 	$dotbitquery= str_replace(".bit", "", $querystripwww); 
 	
 	// TESTING CODE BELOW
+	
 	echo "<b>Output after .bit removed</b>";
 	echo "<br><br>";
 	echo $dotbitquery;
 	
 	// TESTING CODE ENDS
 	
+	
+	// TESTING CODE BELOW
+	
+	echo "<b>JSON response for $dotbitquery</b>";
+	echo "<br><br>";
+	
 	$dotbitqueryresult= shell_exec("sudo /usr/bin/namecoind name_show d/$dotbitquery 2>&1");
 	echo "<pre>$dotbitqueryresult</pre>";
+	
+	// TESTING CODE ENDS
 	
 	$json= $dotbitqueryresult;
 	$decodedjson= json_decode($json);
