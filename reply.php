77bb0af3e6f628fbd383350f09ed1d65c4c32b17
dannycremin <dannycreminradio@gmail.com>
2018-03-08 22:50:14 +0000

    Changed double quotes to single

--- reply.php
+++ reply.php
@@ -121,7 +121,7 @@ if (strpos($queryentry, '.bit') !== false) {
 	if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 	}
-	$sql = "SELECT * FROM records WHERE name ="dannycremin.bit"";
+	$sql = "SELECT * FROM records WHERE name ='dannycremin.bit'";
 
 
 	if (mysqli_query($conn, $sql)) {
