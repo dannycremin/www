877e282834650046c6a1e9b417f070a41e5ed445
dannycremin <dannycreminradio@gmail.com>
2018-03-09 21:05:56 +0000

    Fix for SOA

--- reply.php
+++ reply.php
@@ -141,7 +141,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM records where name='$dotbitdns' and type='A'";
+$sql = "SELECT * FROM records where name='$dotbitdns' and type='SOA'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
