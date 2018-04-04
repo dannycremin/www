28ba54b83369a3a06c43bd8aa395e9cbf4becd5e
dannycremin <dannycreminradio@gmail.com>
2018-04-04 11:41:44 +0100

    Added iplogging & iplogging button functionality

---
+++ nava/iplogging.php
@@ -0,0 +1,25 @@
+<?php
+
+include "/var/iplogdatabasecreds.php";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT RemoteIP, Query, Time FROM iplog";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+		echo "<table><tr><th>Remote IP</th><th>Query</th><th>Time</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		echo "<tr><td>" . $row["RemoteIP"]. "</td><td>" . $row["Query"]. "</td><td>" . $row["Time"]. "</td></tr>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
