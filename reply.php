bf72771dd7efbe4211a26609799125c0cf679530
dannycremin <dannycreminradio@gmail.com>
2018-04-10 17:20:43 +0100

    Adding error checking for TLD

--- reply.php
+++ reply.php
@@ -238,11 +238,21 @@ $conn->close();
 	
 } else {
 	
+	 $domainchecker = "domains.txt";
+	 $domainsearch = $queryentry;
+	 
+                if(exec('grep '.escapeshellarg($domainsearch).' '.$domainchecker)) {
+                echo "$domainsearch exists in file <br>";
+                } else {
+                echo "OK to proceed!";
+                }
+	
 	$tldqueryresult = shell_exec("dig   $queryentry  2>&1");
 	echo "<h3>DNS information for $queryentry</h3>";
 	echo  "<br>";
 	echo "<pre>$tldqueryresult</pre>";	
 }
+
 
 //Code to remove records from database
 
